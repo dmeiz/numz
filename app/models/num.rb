566b0f2b0195530115ee91ab4da9e9b1387124d8
dan <dan@methodhead.com>
2008-03-12 21:05:51 -0400

    Increased size of link property

--- app/models/num.rb
+++ app/models/num.rb
@@ -2,7 +2,7 @@ class Num < DataMapper::Base
   property :value, :float
   property :relevant_at, :datetime
   property :relevant_to, :datetime
-  property :link, :string
+  property :link, :string, :length => 255
 
   has_one :unit 
   belongs_to :user
